1d88151dcec7f44e68929b5d543fd85e581dde18
Ostap95 <ostap_95@hotmail.com>
2017-02-17 15:58:23 +0000

    added second example to bigO

--- BigO/examples.java
+++ BigO/examples.java
@@ -13,7 +13,15 @@ void foo(int[] array) {
   }
   System.out.println(sum + ",  " + product);
 }
+/* This will take O(N) time. The fact that we iterate through the array twice doesn't matter. */
 
-/*
-This will take O(N) time. The fact that we iterate through the array twice doesn't matter.
-*/
+void printPairs(int[] array) {
+  for (int i = 0; i < array.length; i++) {
+    for (int j = 0; j < array.length; j++) {
+      System.out.println(array[i] + "," + array[j]);
+    }
+  }
+}
+ /* The inner for loop has O(N) iterations and it is called N times. Therefore, the runtime is O(N^2).
+ Another way we can see ths is by inspecting what the meaning of the code is. is is printing all pairs (two-element sequences).
+ Therefore are O(N^2) pairs; Therefore the runtime is O(N^2). */
